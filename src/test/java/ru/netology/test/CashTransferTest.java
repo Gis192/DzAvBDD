8da4f46cfd761c20058795a9872618f6ff6fd87e
Ivan Golikov <gis92@rambler.ru>
2023-03-19 16:45:23 +0300

    save2

--- src/test/java/ru/netology/test/CashTransferTest.java
+++ src/test/java/ru/netology/test/CashTransferTest.java
@@ -1,4 +1,57 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataGenerator;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+import ru.netology.page.TransferPage;
+import ru.netology.page.VerificationPage;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class CashTransferTest {
+    LoginPage loginPage;
+    VerificationPage verificationPage;
+    DashboardPage dashboardPage;
+    TransferPage transferPage;
+
+    @BeforeEach
+    void setup () {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataGenerator.getAuthInfo();
+        verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataGenerator.getVerificationCode();
+        dashboardPage = verificationPage.validCode(verificationCode);
+    }
+
+    @Test
+    void pageObjectTest() {
+        //var loginPage = open("http://localhost:9999", LoginPage.class);
+       // var authInfo = DataGenerator.getAuthInfo();
+       // verificationPage = loginPage.validLogin(authInfo);
+       // var verificationCode = DataGenerator.getVerificationCode();
+        // dashboardPage = verificationPage.validCode(verificationCode);
+        var firstCardBalance = DataGenerator.getFirstCardBalance();
+        var secondCardBalance = DataGenerator.getSecondCardBalance();
+        var firstCardInfo = dashboardPage.getCardBalance(firstCardBalance);
+        var secondCardInfo = dashboardPage.getCardBalance(secondCardBalance);
+        var amount = DataGenerator.generateValidAmount(firstCardInfo);
+        var expectedBalanceOfFirstCard = firstCardInfo - amount;
+        var expectedBalanceOfSecondCard = secondCardInfo + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardBalance);
+        var dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardBalance);
+        var actualBalanceOfFirstCard = dashboardPage.getCardBalance(firstCardBalance);
+        var actualBalanceOfSecondCard = dashboardPage.getCardBalance(secondCardBalance);
+        assertEquals(expectedBalanceOfFirstCard, actualBalanceOfFirstCard);
+        assertEquals(expectedBalanceOfSecondCard, actualBalanceOfSecondCard);
+
+    }
+
+
+
+
 }
+
